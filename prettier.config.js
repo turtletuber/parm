7c0fc5abc44d5efeb1ef8069e9ed2b5ba149c8a2
Patrick Michaelsen <mchpatr@amazon.com>
2020-01-21 17:14:22 -0700

    feature: add nrwl

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 80,
-  trailingComma: 'all',
-  singleQuote: true,
-  useTabs: false,
-  tabWidth: 2,
-  semi: true,
-  endOfLine: 'lf',
-};
--- src/index.spec.ts
+++
@@ -1,1 +0,0 @@
-it('runs tests', () => expect(true).toBeTruthy());
