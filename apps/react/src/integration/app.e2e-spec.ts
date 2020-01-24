e2614c2e83f4014eba4c14c3832df640bc878b5a
Patrick Michaelsen <mchpatr@amazon.com>
2020-01-24 15:32:01 -0700

    temp

--- apps/react/src/integration/app.e2e-spec.ts
+++ apps/react/src/integration/app.e2e-spec.ts
@@ -1,6 +1,5 @@
 const storyRoute = '/iframe.html?id=useviewportsize--default';
-const isHeadless = Cypress.env('headless');
-describe('react-playground', () => {
+describe('react-use-viewport-size', () => {
   
   beforeEach(() => {
     cy.viewport(1024, 768);
@@ -9,11 +8,6 @@ describe('react-playground', () => {
  
   it('should display viewport dimensions', () => {
     cy.get('.app').contains('1024px / 768px');
-    if (isHeadless === true) {
-      console.log('its headless');
-      cy.matchImageSnapshot();
-    } else {
-      console.log('its not headless');
-    }
+    cy.matchImageSnapshot();
   });
 });
