87dc2a697bb0499404d98f958efff7279cbc1c5d
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2020-04-14 13:57:24 -0700

    move-it feature: add pm2 ecosystem

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@
 # https://askubuntu.com/a/816
 
 cd "$(dirname "$0")"
+pm2 stop all 
+pm2 delete all
 git fetch origin
 git reset --hard origin/master 
 npm i
-pm2 restart all
-
+pm2 start ./pm2/ecosystem.config.js
+pm2 save
---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  apps: [
+    {
+      name: 'greenroom-rest-api',
+      cwd: '/home/prmichaelsen/workplace/ts-npm-template',
+      script: 'ng',
+      args: 'run greenroom-rest-api:serve:production',
+      autorestart: true,
+      max_memory_restart: '1G',
+      out_file: '/dev/null',
+      error_file: '/dev/null'
+    },
+    {
+      name: 'greenroom-ui',
+      cwd: '/home/prmichaelsen/workplace/ts-npm-template',
+      script: 'ng',
+      args: 'run greenroom-ui:production --prod --allowed-hosts greenroomfinder.app,www.greenroomfinder.app',
+      autorestart: true,
+      max_memory_restart: '1G',
+      out_file: '/dev/null',
+      error_file: '/dev/null'
+    },
+    {
+      name: 'names-not-numbers',
+      cwd: '/home/prmichaelsen/workplace/ts-npm-template',
+      script: 'ng',
+      args: 'run names-not-numbers:serve:production --allowed-hosts namesnotnumbers.app,www.namesnotnumbers.app',
+      autorestart: true,
+      max_memory_restart: '1G',
+      out_file: '/dev/null',
+      error_file: '/dev/null'
+    },
+    {
+      name: 'move-it',
+      cwd: '/home/prmichaelsen/workplace/ts-npm-template',
+      script: 'ng',
+      args: 'run move-it-app:serve:production --allowed-hosts moveit.parm.app,www.moveit.parm.app',
+      autorestart: true,
+      max_memory_restart: '1G',
+      out_file: '/dev/null',
+      error_file: '/dev/null'
+    },
+  ]
+};
