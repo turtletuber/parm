70505d0d899c69a80480d1f616810a0acc6ce218
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2020-04-26 19:41:16 -0700

    f5 feature: allow app/stage config

--- apps/f5/src/environments/environment.ts
+++ apps/f5/src/environments/environment.ts
@@ -1,6 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
 
-export const environment = {
-  production: false
+import { Enviroment } from './environment.interface';
+
+export const environment: Enviroment = {
+  app: 'f5',
+  stage: 'prod',
 };
