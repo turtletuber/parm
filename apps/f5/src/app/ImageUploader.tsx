1886fde220d94357ffab406095b1fc0b10e318d3
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2020-06-04 23:51:05 -0700

    image preview works

--- apps/f5/src/app/ImageUploader.tsx
+++ apps/f5/src/app/ImageUploader.tsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
 import ImageUploader from 'react-images-upload';
+import Grid from '@material-ui/core/Grid';
+  
+type onChange = (files: File[], pictures: string[]) => void
+const initialState: File[] = [];
  
 export const ImgUploader = () => { 
-  const [state, setState] = useState([]);
-  const onDrop = (picture) => {
+  const [state, setState] = useState(initialState);
+  const onDrop: onChange = (pictures) => {
     setState([
-      ...state, picture
+      ...pictures
     ]);
   }
 
   return (
+    <>
     <ImageUploader
       withIcon={true}
       buttonText='Choose images'
@@ -17,5 +22,18 @@ export const ImgUploader = () => {
       imgExtension={['.jpg', '.gif', '.png', '.gif']}
       maxFileSize={5242880}
     />
+    <Grid container>
+      {state.map(f => {
+        return (
+          <Grid item xs={4} >
+            <img
+              src={URL.createObjectURL(f)}
+              style={{ width: '100%' }}
+            />
+          </Grid>
+        );
+      })}
+    </Grid>
+    </>
   );
 }
