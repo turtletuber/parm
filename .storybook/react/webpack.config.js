38f3c4260bce8980102b7895f84b19a1066b6ff0
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2020-11-03 19:19:32 -0700

    bugfix: allow storybook to include other libs

--- .storybook/react/webpack.config.js
+++ .storybook/react/webpack.config.js
@@ -1,7 +1,16 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
   config.resolve.extensions.push('.tsx');
   config.resolve.extensions.push('.ts');
+
+  // https://github.com/nrwl/nx/issues/2320
+  const tsPaths = new TsconfigPathsPlugin();
+  config.resolve.plugins
+    ? config.resolve.plugins.push(tsPaths)
+    : (config.resolve.plugins = [tsPaths]);
+
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
