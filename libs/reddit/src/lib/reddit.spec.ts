a2918c058c68758919c7030786850b3e59a8e252
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2020-11-03 18:08:20 -0700

    feature: demo reddit specs
    * create some terrible reddit specs with snoowrapper that show sample cross post
    * not only are types completely wrong, but they have this issue: https://github.com/not-an-aardvark/snoowrap/issues/221

--- libs/reddit/src/lib/reddit.spec.ts
+++ libs/reddit/src/lib/reddit.spec.ts
@@ -1,7 +1,46 @@
+import Snoowrap, { Submission, Subreddit } from 'snoowrap';
 import { reddit } from './reddit';
 
 describe('reddit', () => {
   it('should work', async () => {
     expect(await (await reddit().getDefaultSubreddits()).length).toBeGreaterThan(0);
+  });
+  it('should fetch subreddit', async () => {
+    const snoo = reddit();
+    const wholesome: Subreddit = await (snoo.getSubreddit('WholesomeAsHeck') as any);
+    expect(wholesome).not.toBeNull();
+  });
+  it.skip('should submit', async () => {
+    const snoo = reddit();
+    const submission: Submission = await (snoo.submitSelfpost({
+      subredditName: 'WholesomeAsHeck',
+      title: 'test',
+      text: 'test',
+    }) as any);
+    expect(submission).not.toBeNull();
+  });
+  it.skip('should submit image', async () => {
+    const snoo = reddit();
+    const wholesome: Subreddit = await (snoo.getSubreddit('WholesomeAsHeck') as any);
+    const submission: Submission = await (wholesome.submitLink({
+      title: 'test 2',
+      url: 'https://firebasestorage.googleapis.com/v0/b/parm-names-not-numbers.appspot.com/o/parm%2Fimages%2Feda049f0-b4fa-11ea-9ad9-af5c0f14df36?alt=media&token=a3d668ec-33d8-45fc-bdf5-8ed387c60c23',
+    } as any) as any);
+    expect(submission).not.toBeNull();
+  });
+  it('should get submission', async () => {
+    const snoo = reddit();
+    const submission: Submission = await (snoo.getSubmission('jnecb9') as any);
+    expect(submission).not.toBeNull();
+  });
+  it.skip('should crosspost submission', async () => {
+    const snoo = reddit();
+    const submission: Submission = await (snoo.getSubmission('jnecb9') as any);
+    console.log(submission);
+    const crosspostResult = await (submission as any).submitCrosspost({
+      subredditName: 'cute',
+      title: 'test',
+    });
+    expect(crosspostResult).not.toBeNull();
   });
 });
--- libs/reddit/src/lib/reddit.ts
+++ libs/reddit/src/lib/reddit.ts
@@ -1,6 +1,6 @@
-import * as snoowrap from 'snoowrap';
+import snoowrap from 'snoowrap';
 import { reddit as redditSecrets } from '@parm/util';
 
 export function reddit() {
   return new snoowrap(redditSecrets);
-} 
+}
